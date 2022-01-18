47657973cf9f52479630b5399563567bf870d444
wx 3eng <62089599+TruckerTaco@users.noreply.github.com>
2022-01-18 14:59:18 -0800

    reupload BurnTree

--- Algorithm/Tree/BurnTree.java
+++ Algorithm/Tree/BurnTree.java
@@ -1,2 +1,74 @@
-package Algorithm.Tree;public class BurnTree {
+package Algorithm.Tree;
+
+import MyUtil.TreeArrayConvert;
+import MyUtil.TreeNode;
+
+import java.util.*;
+
+public class BurnTree {
+    public int timeToBurn(TreeNode root, TreeNode target) {
+        if (root == null || target == null) {
+            return 0;
+        }
+        List<TreeNode> pathToRoot = new ArrayList<>();
+        Set<TreeNode> visited = new HashSet<>();
+        createPathToRoot(root, target, pathToRoot, visited);
+
+        if (pathToRoot.isEmpty()) {
+            return 0;
+        }
+        int result = 0;
+
+        for (int i = 0; i < pathToRoot.size(); i++) {
+            TreeNode node = pathToRoot.get(i);
+            int otherBranchHeight;
+            if (!visited.contains(node.left)) {
+                otherBranchHeight = getHeight(node.left);
+            } else {
+                otherBranchHeight = getHeight(node.right);
+            }
+            result = Math.max(result, otherBranchHeight + i + 1);
+        }
+
+        // code for testing below
+        for (TreeNode elem : pathToRoot) {
+            System.out.print(elem.value + " ");
+        }
+        System.out.print("=>");
+        return result;
+    }
+
+    private void createPathToRoot(TreeNode root, TreeNode target, List<TreeNode> pathToRoot, Set<TreeNode> visited) {
+        if (root == null) {
+            return;
+        }
+        if (pathToRoot.isEmpty() && root != target) {
+            createPathToRoot(root.left, target, pathToRoot, visited);
+            createPathToRoot(root.right, target, pathToRoot, visited);
+            if (!pathToRoot.isEmpty()) {
+                pathToRoot.add(root);
+                visited.add(root);
+            }
+        } else if (root == target) { //&& pathToRoot.isEmpty() == true
+            pathToRoot.add(root);
+            visited.add(root);
+        }
+    }
+
+    private int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = getHeight(root.left);
+        int right = getHeight(root.right);
+        return Math.max(left, right) + 1;
+    }
+
+    public static void main(String[] args) {
+        BurnTree result = new BurnTree();
+        TreeArrayConvert converter = new TreeArrayConvert();
+        TreeNode root1 = converter.constructTree(new Integer[]{0, 1, 2, 3, 4, 5, 6});
+        TreeNode root2 = converter.constructTree(new Integer[]{0, 1, 2, 3, 4, 5, 6, 7});
+        System.out.println(result.timeToBurn(root1, root1.right.left));
+    }
 }
